bd1f366f0829e692dbbebae947becb8aad1eb288
Ogundari <a9014005@hallam.shu.ac.uk>
2023-11-17 09:07:50 +0000

    change to js

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+
+
+(function(){
+document.getElementById("myHeading").innerHTML = "Ivision";
+document.querySelector('nav ul li').setAttribute('class', 'currentPage');
+document.querySelector('nav ul li a').setAttribute('href', 'http://www.google.co.uk');
+})();
