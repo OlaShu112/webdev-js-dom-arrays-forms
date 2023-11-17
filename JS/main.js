3b1936d3919c132b0b90178188896784f4edddeb
Ogundari <a9014005@hallam.shu.ac.uk>
2023-11-17 10:58:57 +0000

    Changes to js code

--- JS/main.js
+++ JS/main.js
@@ -4,18 +4,27 @@
 document.getElementById("myHeading").innerHTML = "Ivision";
 document.querySelector('nav ul li').setAttribute('class', 'currentPage');
 document.querySelector('nav ul li a').setAttribute('href', 'http://www.google.co.uk');
- document.querySelector(".red").addEventListener('click', function(){
- document.querySelector('body').setAttribute('class', "redBack");
- });
- document.querySelector(".blue").addEventListener('click', function(){
- document.querySelector('body').setAttribute('class', "blueBack");
- });
- document.querySelector(".green").addEventListener('click', function(){
- document.querySelector('body').setAttribute('class', "greenBack");
- });
- document.querySelector(".reset").addEventListener('click', function(){
- document.querySelector('body').removeAttribute("class");
- });
+ let colourButtons = document.querySelectorAll(".colPicker");
+ for (let i = 0; i < colourButtons.length; i++) {
+     colourButtons[i].addEventListener("click", chgColour)
+ }
+    const bodyElement = document.querySelector("body")
+    function chgColour(ev){
+ console.dir(ev.target.classList)
+ let colourPicked = ev.target.classList[0] + "Back"
+ bodyElement.setAttribute("class",colourPicked);
+}
+let imageAr = ['images/view1.jpg', 'images/view2.jpg', 'images/view3.jpg', 
+'images/view4.jpg', 'images/view5.jpg', 'images/view6.jpg'];
 
+let imageCount = 0
+setInterval(function() {
+    imageSelect = imageCount - (Matrh.trunc(imageCount / imageAr.length))
+    console.dir(imageSelect);
 
+document.getElementById('myImages').setAttribute('src', imageAr [imageSelect]);
+imageCount += 1;
+}, 1000);
 })();
+
+
