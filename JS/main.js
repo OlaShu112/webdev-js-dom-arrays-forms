c5c1439b3b75dc1335ac67dae2666412f6387307
Ogundari <a9014005@hallam.shu.ac.uk>
2023-11-17 09:49:30 +0000

    Changes to js

--- JS/main.js
+++ JS/main.js
@@ -4,4 +4,18 @@
 document.getElementById("myHeading").innerHTML = "Ivision";
 document.querySelector('nav ul li').setAttribute('class', 'currentPage');
 document.querySelector('nav ul li a').setAttribute('href', 'http://www.google.co.uk');
+ document.querySelector(".red").addEventListener('click', function(){
+ document.querySelector('body').setAttribute('class', "redBack");
+ });
+ document.querySelector(".blue").addEventListener('click', function(){
+ document.querySelector('body').setAttribute('class', "blueBack");
+ });
+ document.querySelector(".green").addEventListener('click', function(){
+ document.querySelector('body').setAttribute('class', "greenBack");
+ });
+ document.querySelector(".reset").addEventListener('click', function(){
+ document.querySelector('body').removeAttribute("class");
+ });
+
+
 })();
